89d7f2e097b92371e4d5aac937f2388214656091
yadig69 <yadig69design@gmail.com>
2021-01-12 18:32:07 -0500

    redux actions and reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,19 @@
-import { createStore } from "redux";
-import { Reducer, initialState } from "./reducer";
+/* eslint-disable no-unused-vars */
+import { createStore, combineReducers } from "redux";
+import { Campsites } from "./campsites";
+import {Comments} from './comments'
+import {Partners} from './partners'
+import {Promotions} from './promotions'
 
 export const ConfigureStore = () => {
-  const store = createStore(Reducer, initialState);
+  const store = createStore(
+    combineReducers({
+      campsites: Campsites,
+      comments: Comments,
+      partners: Partners,
+      promotions: Promotions,
+    })
+  );
 
   return store;
 };
